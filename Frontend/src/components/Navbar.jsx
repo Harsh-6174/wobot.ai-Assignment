81f29801e82df739e9e1074f672836c92d26d32d
Harsh <harshprsh.14@gmail.com>
2024-04-09 20:40:20 +0530

    .

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png'; // Adjust the path to the image
+import logo from '../assets/logo.png'; 
 
 function Navbar() {
   return (
